e9d51296b0971fc8a1751e1ac06d1efcc2d29d83
Alexandr Korinskiy <C:\Users\\AppData\Roaming\The Bat!>
2018-09-12 09:06:59 +0300

    update check winner algorithm with nockecking diagonals with length < 4

--- src/CheckWinFunc/Matches.js
+++ src/CheckWinFunc/Matches.js
@@ -6,13 +6,13 @@ export default matches;
  */
 
 function matches(x, y, cells) {
-  //debugger;
   let width = cells.length;
   let height = cells[x].length;
   let player = cells[x][y]; //player ID
   let count = 0;
 
   // Vertical check
+  //iterate over current column in all rows
   for (let i = 0; i < height; i++) {
     if (cells[x][i] === player) {
       count++;
@@ -25,6 +25,7 @@ function matches(x, y, cells) {
   }
   count = 0;
   //Horizontal check
+  //iterate over current row in all columns
   for (let i = 0; i < width; i++) {
     if (cells[i][y] === player) {
       count++;
@@ -37,39 +38,36 @@ function matches(x, y, cells) {
   }
   count = 0;
   //diagonal check
-  let xMinTop;
-  let yMinTop;
-  let xMinBottom;
-  let yMinBottom;
+  let xTopLeft;
+  let yTopLeft;
+  let xBottomLeft;
+  let yBottomLeft;
   //defined top left begin of diagonal
-  for (let col = x, row = y; row > -1 && col > -1; row--, col--) {
-    if (col < 1) {
-      xMinTop = col;
-      yMinTop = row;
-    }
-    if (row < 1) {
-      xMinTop = col;
-      yMinTop = row;
+  for (let col = x, row = y; col > -1 && row > -1; col--, row--) {
+    if (col == 0 || row == 0) {
+      xTopLeft = col;
+      yTopLeft = row;
     }
   }
   //defined bottom left begin of diagonal
-  for (let col = x, row = y; row < height && col > -1; row++, col--) {
-    if (col < 1) {
-      xMinBottom = col;
-      yMinBottom = row;
-    }
-    if (row > height - 2) {
-      xMinBottom = col;
-      yMinBottom = row;
+  for (let col = x, row = y; col > -1 && row < height; col--, row++) {
+    if (col == 0 || row > height - 2) {
+      xBottomLeft = col;
+      yBottomLeft = row;
     }
   }
 
   //check to bottom right diagonal
   for (
-    let col = xMinTop, row = yMinTop;
-    row < height && col < width;
-    row++, col++
+    let col = xTopLeft, row = yTopLeft;
+    col < width && row < height;
+    col++, row++
   ) {
+    //erly exit from loop if diagonal.length < 4 because
+    //it locate in top right or bottom left corner of board
+    if (xTopLeft > width - 4 || yTopLeft > height - 4) {
+      break;
+    }
     if (cells[col][row] === player) {
       count++;
     } else {
@@ -82,10 +80,15 @@ function matches(x, y, cells) {
   count = 0;
   //check to top right diagonal
   for (
-    let col = xMinBottom, row = yMinBottom;
-    row < height && col < width;
-    row--, col++
+    let col = xBottomLeft, row = yBottomLeft;
+    col < width && row > -1;
+    col++, row--
   ) {
+    //erly exit from loop if diagonal.length < 4 because
+    //it locate in top left or bottom right corner of board
+    if (xBottomLeft > width - 4 || yBottomLeft < 3) {
+      break;
+    }
     if (cells[col][row] === player) {
       count++;
     } else {
